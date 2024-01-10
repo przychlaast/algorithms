1f87009847b6a15b74423835d6fdffb669a1e3a4
przychlast <65180822+przychlaast@users.noreply.github.com>
2024-01-10 15:05:52 +0100

    Dodanie folderu lista_5

---
+++ lista_5/zadania1_5.py
@@ -0,0 +1,122 @@
+import random
+import time
+
+#zadanie 1
+def generator_zajec(n):
+    s = [random.uniform(0, 24) for _ in range(n)]
+    f = [si + random.uniform(0.5, 2) for si in s]
+    
+    zajecia = list(zip(s, f))
+    zajecia.sort(key=lambda x: x[1])
+    
+    s_sorted, f_sorted = zip(*zajecia)
+    s_sorted = list(s_sorted)
+    f_sorted = list(f_sorted)
+    
+    return s_sorted, f_sorted
+
+s = [1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12]
+f = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+s_test, f_test = generator_zajec(100)
+
+#zadanie 2
+def activity_selector_dp(s, f):
+    n = len(f)
+    dp = [0]*(n+1)
+
+    f = [0] + f
+    s = [0] + s
+
+    for i in range(1, n+1):
+        dp[i] = max(dp[j] + 1 for j in range(i) if s[i] >= f[j])
+    return dp[n]
+
+
+print(f'Zadanie 2 - Liczba przydzielonych zajęć algorytm dynamiczny: {activity_selector_dp(s, f)}') 
+print(f'Zadanie 2 - Liczba przydzielonych zajęć dla losowych 50 zajęć algorytm dynamiczny: {activity_selector_dp(s_test, f_test)}')
+
+#zadanie 3
+def recursive_activity_selector(s, f, k, n):
+    m = k + 1
+    while m <= n and s[m] < f[k]:
+        m += 1
+    if m <= n:
+        return [k] + recursive_activity_selector(s, f, m, n)
+    else:
+        return [k]
+
+def activity_selector(s, f):
+    n = len(s)
+    A = [0]
+    j = 0
+    for i in range(1, n):
+        if s[i] >= f[j]:
+            A.append(i)
+            j = i
+    return A
+
+
+print(f'Zadanie 3 - Algorytm rekurencyjny: {recursive_activity_selector(s, f, 0, len(s)-1)}')
+print(f'Zadanie 3 - Algorytm iteracyjny: {activity_selector(s, f)}')
+
+
+print(f'Zadanie 3 - Algorytm rekurencyjny dla losowych 50 zajęć: {recursive_activity_selector(s_test, f_test, 0, len(s_test)-1)}')
+print(f'Zadanie 3 - Algorytm iteracyjny dla losowych 50 zajęć: {activity_selector(s_test, f_test)}')
+
+algorytmy = [activity_selector_dp, recursive_activity_selector, activity_selector]
+
+print('Zadanie 3 - ')
+for algorytm in algorytmy:
+    if algorytm == recursive_activity_selector:
+        start = time.time()
+        algorytm(s_test, f_test, 0, len(s)-1)
+        koniec = time.time()
+    else:
+        start = time.time()
+        algorytm(s_test, f_test)
+        koniec = time.time()
+    print(f'Czas działania {algorytm.__name__}: {koniec-start}')
+    
+#zadanie 4
+def dynamic_coin_changing(nominaly, sum):
+
+    n = len(nominaly)
+    dp = [[0]*(sum+1) for _ in range(n+1)]
+    dp[0] = list(range(sum+1))
+
+    for i in range(1, n+1):
+        for j in range(0, sum+1):
+            if nominaly[i-1] > j:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = min(dp[i][j-nominaly[i-1]] + 1, dp[i-1][j])
+    return dp[n][sum]
+
+nominaly = [1, 5, 8, 10, 20, 50]
+print(f'Zadanie 4 - Minimalna liczba monet za pomocą których możemy wydać wejściową sumę: {dynamic_coin_changing(nominaly, 100)}')
+
+
+#zadanie 5
+def coin_changing(nominaly, sum):
+    n = len(nominaly)
+    wynik = []
+    for i in range(n-1, -1, -1):
+        while sum >= nominaly[i]:
+            sum -= nominaly[i]
+            wynik.append(nominaly[i])
+    return wynik
+
+print(f'Zadanie 5 - Nominały za pomocą których możemy wydać wejściową sumę: {coin_changing(nominaly, 100)}')
+
+nominaly_test = [1, 3, 4]
+sum = 1
+while True:
+    if dynamic_coin_changing(nominaly_test, sum) == len(coin_changing(nominaly_test, sum)):
+        sum += 1
+    else:
+        print("Zadanie 5 - \nDla sum =", sum)
+        print("dynamic_coin_changing:", dynamic_coin_changing(nominaly_test, sum))
+        print("greedy_coin_changing:", len(coin_changing(nominaly_test, sum)), coin_changing(nominaly_test, sum))
+        print("Nominały dla których algorytm zachłanny nie znajduje optymalnego rozwiązania:", nominaly_test)
+        break
